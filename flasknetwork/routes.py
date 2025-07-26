26579e9596e9a20c6ff9485c9756fb41170e0faa
aryan <aryanleo055@gmail.com>
2025-07-26 17:05:19 +0200

    updating profile/account into

--- flasknetwork/routes.py
+++ flasknetwork/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from flasknetwork import app, db, bcrypt
-from flasknetwork.forms import RegistrationForm, LoginForm
+from flasknetwork.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flasknetwork.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -60,7 +60,18 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account')
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account')) # post-get-redirect pattern
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
