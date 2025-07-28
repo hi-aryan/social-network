96015c66dfc82a5f47e364317a89cebc4279c678
aryan <aryanleo055@gmail.com>
2025-07-28 15:18:51 +0200

    password reset via gmail

--- flasknetwork/__init__.py
+++ flasknetwork/__init__.py
@@ -1,16 +1,26 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
+load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey69'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASS')
+mail = Mail(app)
 
 # import after app is defined to avoid circular import
 from flasknetwork import routes
