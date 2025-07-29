b38774a4416dbc89844631d29ed24e5e69752406
aryan <aryanleo055@gmail.com>
2025-07-29 16:10:26 +0200

    using flask blueprints + moved env variables to windows system settings

--- flasknetwork/__init__.py
+++ flasknetwork/__init__.py
@@ -1,26 +1,25 @@
-import os
-from dotenv import load_dotenv
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flasknetwork.config import Config
 
-load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASS')
 mail = Mail(app)
 
 # import after app is defined to avoid circular import
-from flasknetwork import routes
+from flasknetwork.users.routes import users
+from flasknetwork.posts.routes import posts
+from flasknetwork.main.routes import main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
