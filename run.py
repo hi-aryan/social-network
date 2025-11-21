e8dac6b62e0a8955595344f8df7f921d00992a30
aryan <legto3@gmail.com>
2025-11-21 11:59:57 +0100

    yes

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 from flasknetwork import create_app
 
 app = create_app()
@@ -5,4 +10,4 @@ app = create_app()
 # if file is run directly only
 if __name__ == '__main__':
     print(f"SECRET_KEY is set: {app.config.get('SECRET_KEY') is not None}")
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
