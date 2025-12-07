90f751ab8fcfdc6580eecda36d645453fea01681
aryan <legto3@gmail.com>
2025-12-07 16:11:12 +0100

    display all 3 tags + opacity changes for tags (clean up the logic if possible) + new bg

---
+++ tags.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+load_dotenv()
+  # seeding after migration
+from flasknetwork import db, create_app
+from flasknetwork.models import Tag, TagSentiment
+   
+app = create_app()
+with app.app_context():
+    tags_data = [
+        ("Group Projects", TagSentiment.positive),
+        ("Clear Grading Criteria", TagSentiment.positive),
+        ("Lots of Reading", TagSentiment.negative),
+        ("Entertaining Lectures", TagSentiment.positive),
+        ("Textbook Required", TagSentiment.negative),
+        ("Tough Grading", TagSentiment.negative),
+        ("Bamboozling Exams", TagSentiment.negative),
+        ("Friendly TAs", TagSentiment.positive),
+        ("Professor & TAs are Accessible Outside Class", TagSentiment.positive),
+        ("Recorded Lectures", TagSentiment.positive),
+        ("LOTS of Assignments", TagSentiment.negative),
+        ("Bad Course Layout", TagSentiment.negative),
+        ("Lecture Heavy", TagSentiment.negative),
+        #("Attendance Mandatory", TagSentiment.negative),
+        ("Industry Relevant", TagSentiment.positive),
+        ("Outdated Content", TagSentiment.negative),
+    ]
+
+    for name, sentiment in tags_data:
+        tag = Tag(name=name, sentiment=sentiment)
+        db.session.add(tag)
+    db.session.commit()
